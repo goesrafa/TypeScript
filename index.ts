983def443895f606a119d3cc6c219fc4258689f3
goesrafa <rafa.goes.2012@gmail.com>
2022-02-27 16:56:10 -0300

    Interface e Type

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log('TypeScript');
+//types - define uma estrutura de dados
+//interfaces - contratos para implementação das classes
+
+interface Ianimal{
+    nome: string;
+    tipo: 'terrestre' | 'Aquatico';
+    domestico :boolean;
+}
+
+interface Ifelinos extends Ianimal {
+    visaoNoturna: boolean,
+}
+
+interface Icanino extends Ianimal {
+    porte: 'pequeno' | 'medio' | 'grande';
+}
+
+type Idomestico = Ifelinos | Icanino;
+
+const animal: Idomestico = {
+    domestico: true,
+    nome: 'cachorro',
+    porte: 'medio',
+    tipo: "terrestre",
+}
+
+
+
